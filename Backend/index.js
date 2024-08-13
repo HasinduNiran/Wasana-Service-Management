127271852ed396c3e9f2520301fc6d89f793de44
Niran B G S H <127426411+HasinduNiran@users.noreply.github.com>
2024-08-13 17:15:31 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -9,8 +9,26 @@ import { PORT, mongoDBURL } from './config.js';
 
 // Importing routes
 
-// import UserAccount_Route from './Routes/UserAccount_Route.js';
-// import Employee_Route from './Routes/Employee_Route.js';
+
+ import Employee_Route from './Routes/Employee_Route.js';
+ //import Applicant_Route from './Routes/Applicant_Route.js';
+ //import Booking_Route from './Routes/Booking_Route.js';
+ //import EmployeeSalary_Route from './Routes/EmployeeSalary_Route.js';
+ //import Feedback_Route from './Routes/Feedback_Route.js';
+ //import Inquire_Route from './Routes/inquire_Route.js';
+ //import Promotion_Route from './Routes/Promotion_Route.js';
+ //import Repair_Route from './Routes/Repair_Route.js';
+ //import ServiceHistory_Route from './Routes/ServiceHistory_Route.js';
+// import Store _Route from './Routes/Store _Route.js';
+//import Vacancy_Route  from './Routes/Vacancy_Route.js';
+//import Vehicle_Route from './Routes/Vehicle_Route.js';
+
+
+
+ 
+
+ 
+
 
 
 // Creating an instance of the Express application
@@ -24,7 +42,18 @@ app.use(cors());
 
 // Using routes for endpoints
 
-// app.use('/customer', UserAccount_Route);
+ //app.use('/Employee', Employee_Route);
+ //app.use('/Applicant', Applicant_Route);
+ //app.use('/Booking', Booking_Route);
+ //app.use('/EmployeeSalary', EmployeeSalary_Route);
+ //app.use('/Feedback', Feedback_Route);
+// app.use('/inquire', Inquire_Route);
+ //app.use('/Promotion', Promotion_Route);
+ //app.use('/Repair', Repair_Route);
+ //app.use('/ServiceHistory', ServiceHistory_Route);
+ // app.use('/Store', Store_Route);
+ //app.use('/Vacancy', Vacancy_Route);
+ //app.use('/Vehicle', Vehicle_Route);
 
 
 // Connecting to the MongoDB database
