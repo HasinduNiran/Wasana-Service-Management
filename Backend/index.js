8a2b4408c2159aa416299f099af140e0319b4800
SithagiV <153867463+SithagiV@users.noreply.github.com>
2024-08-13 18:22:30 +0530

    repair and vacancy

--- Backend/index.js
+++ Backend/index.js
@@ -17,11 +17,11 @@ import { PORT, mongoDBURL } from './config.js';
  //import Feedback_Route from './Routes/Feedback_Route.js';
  //import Inquire_Route from './Routes/inquire_Route.js';
  //import Promotion_Route from './Routes/Promotion_Route.js';
- //import Repair_Route from './Routes/Repair_Route.js';
- //import ServiceHistory_Route from './Routes/ServiceHistory_Route.js';
-// import Store _Route from './Routes/Store _Route.js';
-//import Vacancy_Route  from './Routes/Vacancy_Route.js';
-//import Vehicle_Route from './Routes/Vehicle_Route.js';
+ import Repair_Route from './Routes/Repair_Route.js';
+ import ServiceHistory_Route from './Routes/ServiceHistory_Route.js';
+//import Store _Route from './Routes/Store _Route.js';
+import Vacancy_Route  from './Routes/Vacancy_Route.js';
+import Vehicle_Route from './Routes/Vehicle_Route.js';
 
 
 
@@ -42,18 +42,18 @@ app.use(cors());
 
 // Using routes for endpoints
 
- //app.use('/Employee', Employee_Route);
+ app.use('/Employee', Employee_Route);
  //app.use('/Applicant', Applicant_Route);
  //app.use('/Booking', Booking_Route);
  //app.use('/EmployeeSalary', EmployeeSalary_Route);
  //app.use('/Feedback', Feedback_Route);
-// app.use('/inquire', Inquire_Route);
+ //app.use('/inquire', Inquire_Route);
  //app.use('/Promotion', Promotion_Route);
- //app.use('/Repair', Repair_Route);
- //app.use('/ServiceHistory', ServiceHistory_Route);
- // app.use('/Store', Store_Route);
- //app.use('/Vacancy', Vacancy_Route);
- //app.use('/Vehicle', Vehicle_Route);
+ app.use('/Repair', Repair_Route);
+ app.use('/ServiceHistory', ServiceHistory_Route);
+ //app.use('/Store', Store_Route);
+ app.use('/Vacancy', Vacancy_Route);
+ app.use('/Vehicle', Vehicle_Route);
 
 
 // Connecting to the MongoDB database
