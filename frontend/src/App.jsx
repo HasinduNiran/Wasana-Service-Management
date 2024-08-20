20ac3a3f77520274cf243fb62e62c16bba3f96d7
Niran B G S H <127426411+HasinduNiran@users.noreply.github.com>
2024-08-20 11:05:25 +0530

    vehicle done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,13 @@ import ShowEmployee from './assets/pages/Employee/ShowEmployee';
 import EditEmployee from './assets/pages/Employee/EditEmployee';
 import DeleteEmployee from './assets/pages/Employee/DeleteEmployee';
 import ReadOneEmployee from './assets/pages/Employee/ReadOneEmployee';
+import { CreateVehicle } from './assets/pages/Vehicle/Createvehicle';
+import ShowAllvehicle from './assets/pages/Vehicle/ShowAllvehicle';
+ 
 const App = () => {
   return (
    <Routes>
+    <Route path ='/Vehicle' element={<ShowAllvehicle/>}/>
     
     <Route path = '/Employee' element = {<ShowEmployee/>}/>
     <Route path = '/Employee/create' element = {<CreateEmployee/>}/>
