b2e5ddf78e963b06f702d4e13edc7783e82f5575
Niran B G S H <127426411+HasinduNiran@users.noreply.github.com>
2024-08-26 22:13:34 +0530

    customer booking add not work

--- Backend/Routes/Customer_Route.js
+++ Backend/Routes/Customer_Route.js
@@ -41,17 +41,34 @@ router.get('/', async (request, response) => {
 });
 
 // Get a single customer
-
-router.get('/:id', async (request, response) => {
+router.get('/:identifier', async (request, response) => {
     try {
-        const customer = await Customer.findById(request.params.id);
+        // Extracting the identifier from the request parameters
+        const { identifier } = request.params;
 
-        if (!customer) return response.status(404).send({ message: 'Customer not found' });
+        // Checking if the provided identifier is a valid MongoDB ObjectId
+        if (mongoose.Types.ObjectId.isValid(identifier)) {
+            // Fetching a vehicle from the database based on the ID
+            const cuByID = await Customer.findById(identifier);
+            if (cuByID) {
+                // Sending the fetched vehicle as a JSON response if found by ID
+                return response.status(200).json(cuByID);
+            }
+        }
 
-        return response.json(customer);
+        // If the provided identifier is not a valid ObjectId, try searching by register number
+        const customerByCUSID = await Customer.findOne({ cusID: identifier });
+        if (customerByCUSID) {
+            // Sending the fetched vehicle as a JSON response if found by register number
+            return response.status(200).json(customerByCUSID);
+        }
+
+        // If no vehicle found by either ID or register number, send a 404 Not Found response
+        return response.status(404).json({ message: 'Customer not found' });
     } catch (error) {
-        console.error(error.message);
-        response.status(500).send({ message: error.message });
+        // Handling errors and sending an error response with detailed error message
+        console.error(error);
+        response.status(500).send({ message: 'Error fetching Customer: ' + error.message });
     }
 });
 
