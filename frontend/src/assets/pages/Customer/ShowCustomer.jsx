581560ddda797e4694457863154aa0ec74e59d94
Niran B G S H <127426411+HasinduNiran@users.noreply.github.com>
2024-09-08 13:32:02 +0530

    customer done

--- frontend/src/assets/pages/Customer/ShowCustomer.jsx
+++ frontend/src/assets/pages/Customer/ShowCustomer.jsx
@@ -275,19 +275,22 @@ const ShowCustomer = () => {
                                     <td className='border px-4 py-2'>{customer.email}</td>
                                     <td className='border px-4 py-2'>{maskPassword(customer.password)}</td>
                                     <td className='border px-4 py-2 flex justify-center items-center space-x-2'>
-                                        <Link to={`/customer/get/${customer._id}`} className="text-blue-500">
+                                        <Link to={`/customer/${customer._id}`} className="text-blue-500">
                                             <BsInfoCircle />
                                         </Link>
                                         <Link to={`/customer/edit/${customer._id}`} className="text-blue-500">
                                             <AiOutlineEdit />
                                         </Link>
-                                        <button
+                                        <Link to={`/customer/delete/${customer._id}`} className="text-blue-500">
+                                        <MdOutlineDelete />
+                                        </Link>
+                                        {/* <button
                                             type="button"
                                             className="text-red-500"
                                             onClick={() => handleDelete(customer._id)}
                                         >
                                             <MdOutlineDelete />
-                                        </button>
+                                        </button> */}
                                     </td>
                                 </tr>
                             ))}
