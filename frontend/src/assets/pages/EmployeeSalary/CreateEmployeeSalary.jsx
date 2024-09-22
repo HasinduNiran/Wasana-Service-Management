50dc7799bfdfcb800f2a5f662b4d99a321d9e092
lasal dissanayake <122669223+LasalDissanayake@users.noreply.github.com>
2024-09-22 17:59:20 +0530

    css and epf resolved

--- frontend/src/assets/pages/EmployeeSalary/CreateEmployeeSalary.jsx
+++ frontend/src/assets/pages/EmployeeSalary/CreateEmployeeSalary.jsx
@@ -146,75 +146,78 @@ const CreateEmployeeSalary = () => {
 
   const styles = {
     container: {
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      minHeight: "100vh",
-      padding: "20px",
-      fontFamily: '"Noto Sans", sans-serif',
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+        padding: "20px",
+        fontFamily: '"Noto Sans", sans-serif',
     },
     backButton: {
-      marginBottom: "50%",
-      marginLeft: "-80%",
-      position: "absolute",
+        marginBottom: "50%",
+        marginLeft: "-80%",
+        position: "absolute",
     },
     image: {
-      borderRadius: "30px",
-      maxWidth: "240px",
-      padding: "0px",
-      height: "585px",
-      borderTopRightRadius: "0px",
-      borderBottomRightRadius: "0px",
+        borderRadius: "30px",
+        maxWidth: "240px",
+        padding: "0px",
+        height: "810px",
+        borderTopRightRadius: "0px",
+        borderBottomRightRadius: "0px",
     },
     form: {
-      borderRadius: "30px",
-      backgroundColor: "#1a1a1a",
-      color: "#fff",
-      maxWidth: "450px",
-      padding: "20px",
-      height: "auto",
-      borderTopLeftRadius: "0px",
-      borderBottomLeftRadius: "0px",
+        borderRadius: "30px",
+        backgroundColor: "#1a1a1a",
+        color: "#fff",
+        maxWidth: "450px",
+        padding: "20px",
+        height: "auto",
+        borderTopLeftRadius: "0px",
+        borderBottomLeftRadius: "0px",
     },
     title: {
-      color: "#6c1c1d",
-      fontSize: "30px",
-      fontWeight: "600",
-      paddingLeft: "30px",
-      position: "relative",
-      display: "flex",
-      alignItems: "center",
+        color: "#6c1c1d",
+        fontSize: "30px",
+        fontWeight: "600",
+        paddingLeft: "30px",
+        position: "relative",
+        display: "flex",
+        alignItems: "center",
     },
     input: {
-      backgroundColor: "#333",
-      color: "#fff",
-      border: "1px solid rgba(105, 105, 105, 0.397)",
-      borderRadius: "10px",
-      fontSize: "1rem",
-      padding: "15px 8px",
-      outline: "0",
-      width: "100%",
-      marginTop: "20px",
-      marginBottom: "20px",
+        backgroundColor: "#333",
+        color: "#fff",
+        border: "1px solid rgba(105, 105, 105, 0.397)",
+        borderRadius: "10px",
+        fontSize: "1rem",
+        padding: "15px 8px",
+        outline: "0",
+        width: "100%",
+        marginTop: "20px",
+        marginBottom: "20px",
     },
     flex: {
-      display: "flex",
-      gap: "8px",
-      marginTop: "15px",
+        display: "flex",
+        gap: "8px",
+        marginTop: "15px",
     },
     submitButton: {
-      border: "none",
-      backgroundColor: "#6c1c1d",
-      marginTop: "10px",
-      outline: "none",
-      padding: "10px",
-      borderRadius: "10px",
-      color: "#fff",
-      fontSize: "16px",
-      width: "100%",
-      cursor: "pointer",
+        border: "none",
+        backgroundColor: "#6c1c1d",
+        marginTop: "10px",
+        outline: "none",
+        padding: "10px",
+        borderRadius: "10px",
+        color: "#fff",
+        fontSize: "16px",
+        width: "100%",
+        cursor: "pointer",
     },
-  };
+    submitButtonHover: {
+        backgroundColor: "#661003f5",
+    },
+};
 
   return (
     <div>
@@ -301,7 +304,7 @@ const CreateEmployeeSalary = () => {
               Calculate OT Pay
             </button>
           </div>
-          <div style={styles.flex}>
+          {/* <div style={styles.flex}>
             <label style={{ color: "#fff", paddingRight: "5px" }}>
               Include EPF:
             </label>
@@ -310,7 +313,7 @@ const CreateEmployeeSalary = () => {
               checked={includeEPF}
               onChange={() => setIncludeEPF(!includeEPF)}
             />
-          </div>
+          </div> */}
           <div style={styles.flex}>
             <input
               type="text"
