60b3bcc4e960e6e274b46f2af6dcc3beceeda55f
lasal dissanayake <122669223+LasalDissanayake@users.noreply.github.com>
2024-09-27 15:55:54 +0530

    Taxige photo ek ain kra

--- frontend/src/assets/pages/Navbar/Navbar.jsx
+++ frontend/src/assets/pages/Navbar/Navbar.jsx
@@ -26,31 +26,10 @@ const Navbar = () => {
           >
             <span className="sr-only">Open user menu</span>
            
-           <img className="w-8 h-8 rounded-full"src={profile} alt="profile"  />
+           
     
           </button>
-          {isDropdownOpen && (
-            <div className="z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
-              <div className="px-4 py-3">
-                <span className="block text-sm text-gray-900 dark:text-white">Sasindu</span>
-                <span className="block text-sm text-gray-500 truncate dark:text-gray-400">sasi@gmail.com</span>
-              </div>
-              <ul className="py-2" aria-labelledby="user-menu-button">
-               
-                <li>
-                  <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-                    Settings
-                  </a>
-                </li>
-              
-                <li>
-                  <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-                    Sign out
-                  </a>
-                </li>
-              </ul>
-            </div>
-          )}
+          
           <button
             data-collapse-toggle="navbar-user"
             type="button"
